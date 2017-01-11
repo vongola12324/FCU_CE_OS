53d86fa38494b0cf3fd8e89e987954fd405c394b
vongola12324 <vongola12324@coder.tw>
2017-01-11 14:54:17 +0800

    Finish HW5

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -1,7 +1,20 @@
-/*
-產生三個child
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-for(i=0; i<3 ;i++) {
-    fork();
+int main(void) {
+    int pid = getpid(), i;
+    for(i=0; i<3; i++) {
+        if (pid > 0) {
+            pid = fork();
+        }
+        if (pid > 0) {
+            printf("I'm the parent! pid = %d\n", getpid());
+        } else if (pid == 0) {
+            printf("I'm the child! pid = %d\n", getpid());
+        } else {
+            perror("Error!");
+        }
+    }
+    return 0;
 }
