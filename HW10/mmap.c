48c8ae1522c63f39405a26231e0ddd5b7377d4e3
vongola12324 <vongola12324@coder.tw>
2017-01-11 17:26:44 +0800

    Fix typo

--- HW10/mmap.c
+++ HW10/mmap.c
@@ -6,8 +6,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-void handle_error(msg) {
-
+void handle_error(char *msg) {
     do { 
         perror(msg);
         exit(EXIT_FAILURE);  
@@ -22,14 +21,14 @@ int main(int argc, char *argv[])
 
     fd = open(argv[1], O_RDONLY);
     fstat(fd, &sb);
-    printf("Size: %lu\n", (uint64_t)sb.st_size);
+    printf("Size: %llu\n", (uint64_t)sb.st_size);
 
     memblock = mmap(NULL, sb.st_size, PROT_WRITE, MAP_PRIVATE, fd, 0);
     if (memblock == MAP_FAILED) handle_error("mmap");
 
     for(uint64_t i = 0; i < 10; i++)
     {
-        printf("[%lu]=%X ", i, memblock[i]);
+        printf("[%llu]=%X\n", i, memblock[i]);
 
     }
     printf("\n");
