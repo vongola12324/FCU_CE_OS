37f37b914c92d05cf87fa46d7967e4b7028b7c70
vongola12324 <vongola12324@coder.tw>
2017-01-11 10:58:27 +0800

    Fix HW1 Bug

--- HW1/hw1.c
+++ HW1/hw1.c
@@ -1,16 +1,18 @@
-/* 
-hello_pthread1.c
-*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
 #define MAXTHREADS 4
+
 char *messages[MAXTHREADS+1]={"Hello 0","Hello 1","Hello 2","Hello 3","Error !!!"};
+
 void *Hello(void *pthreadid) {
     int *idp, id;
     // sleep(1);
     idp = (int *) pthreadid;
     id = *idp;
-    printf(" My Thread's ID [%d] %s\n", id, messages[id]);
+    printf("My Thread's ID [%d] %s\n", id, messages[id]);
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
@@ -18,8 +20,11 @@ int main(int argc, char *argv[]) {
     int id;
     int i;
     for(id = 0; id < MAXTHREADS; id++) {
+        int *id2 = (int*)malloc(sizeof(int));
+        *id2 = id;
         printf("Create thread %d\n", id);
-        pthread_create(&child[id], NULL, Hello, (void *) &id); 
+        pthread_create(&child[id], NULL, Hello, (void *) id2);
+        sleep(1);
     }
 
     /** Wait for all threads to finish **/
